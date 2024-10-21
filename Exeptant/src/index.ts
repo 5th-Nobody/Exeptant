2c117a0281ca6892c0449d34aed5dd599289ef13
5th-Nobody <50621403+5th-Nobody@users.noreply.github.com>
2024-10-21 10:36:02 +0600

    Update index.ts

---
+++ Exeptant/src/index.ts
@@ -0,0 +1,3 @@
+export { ExpectantManager } from './ExpectantManager';
+export { BaseExpectant, IExpectant, IBaseExpectantionProps } from './BaseExpectant';
+export { SpecificationsValueChangedExpectant } from './expectants/SpecificationsValueChangedExpectant';
